0f33791689cd8655cb92b50089dc6d6c8e1b0761
Gaolingx <947770192@qq.com>
2024-01-15 11:22:27 +0800

    20240115_主城UI显示逻辑

--- ET/Unity/Assets/Scripts/UIWindow/MainCityWnd.cs
+++ ET/Unity/Assets/Scripts/UIWindow/MainCityWnd.cs
@@ -1,12 +1,37 @@
 //功能：主城UI界面
+using PEProtocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainCityWnd : WindowRoot
 {
+    public Text txtFight;
+    public Text txtPower;
+    public Image imgPowerPrg;
+    public Text txtLevel;
+    public Text txtName;
+    public Text txtExpPrg;
+
+
     protected override void InitWnd()
     {
         base.InitWnd();
+
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        PlayerData pd = GameRoot.Instance.PlayerData;
+
+        SetText(txtFight, PECommon.GetFightByProps(pd));
+        SetText(txtPower, "体力:" + pd.power + "/" + PECommon.GetPowerLimit(pd.lv));
+        imgPowerPrg.fillAmount = pd.power * 1.0f / PECommon.GetPowerLimit(pd.lv);
+        SetText(txtLevel, pd.lv);
+        SetText(txtName, pd.name);
+
+        //expPrg
     }
 }
