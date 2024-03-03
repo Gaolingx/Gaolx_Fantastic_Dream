848885672ac01968466d32b6fe0ba23338281992
Gaolingx <947770192@qq.com>
2024-03-03 21:40:28 +0800

    20240303_世界聊天系统开发1

---
+++ ET/Unity/Assets/GameMain/Scripts/UIWindow/ChatWnd.cs
@@ -0,0 +1,41 @@
+//功能：聊天界面
+
+using PEProtocol;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ChatWnd : WindowRoot {
+    public InputField iptChat;
+    public Text txtChat;
+    public Image imgWorld;
+    public Image imgGuild;
+    public Image imgFriend;
+
+    private int chatType;
+    private List<string> chatLst = new List<string>();
+
+    protected override void InitWnd() {
+        base.InitWnd();
+
+    }
+
+
+    public void ClickSendBtn() {
+
+    }
+    public void ClickWorldBtn() {
+        audioSvc.PlayUIAudio(Constants.UIClickBtn);
+    }
+    public void ClickGuildBtn() {
+        audioSvc.PlayUIAudio(Constants.UIClickBtn);
+    }
+    public void ClickFriendBtn() {
+        audioSvc.PlayUIAudio(Constants.UIClickBtn);
+    }
+    public void ClickCloseBtn() {
+        audioSvc.PlayUIAudio(Constants.UIClickBtn);
+    }
+}
