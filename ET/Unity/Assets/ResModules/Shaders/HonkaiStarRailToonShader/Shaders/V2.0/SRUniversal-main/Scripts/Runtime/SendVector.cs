6e041b7be106ae3d771aa05cbabaa22f89195969
Gaolingx <947770192@qq.com>
2024-03-05 11:46:31 +0800

    20240305_Update HonkaiStarRailToonShader

--- ET/Unity/Assets/ResModules/Shaders/HonkaiStarRailToonShader/Shaders/V2.0/SRUniversal-main/Scripts/Runtime/SendVector.cs
+++ ET/Unity/Assets/ResModules/Shaders/HonkaiStarRailToonShader/Shaders/V2.0/SRUniversal-main/Scripts/Runtime/SendVector.cs
@@ -24,7 +24,7 @@ public class SendVector : MonoBehaviour
     private void Start()
     {
         InitComponent();
-        //GetMaterialsByReference();
+        GetMaterialsByReference();
     }
 
     public void GetMaterialsByReference()
