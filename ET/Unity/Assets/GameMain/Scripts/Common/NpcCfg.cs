4491645e65c10de819171044512b47b3d58141d3
Gaolingx <947770192@qq.com>
2024-03-15 16:24:06 +0800

    20240315_update map

--- ET/Unity/Assets/GameMain/Scripts/Common/NpcCfg.cs
+++ ET/Unity/Assets/GameMain/Scripts/Common/NpcCfg.cs
@@ -11,16 +11,19 @@ public class NpcCfg : MonoBehaviour
     }
 
     //NPC配置
-    public Vector3 Transform_NpcID_0_Position = new Vector3((float)-20.1362114, (float)-0.615053654, (float)66.0065002);
-    public Vector3 Transform_NpcID_0_Rotation = new Vector3(0, -180, 0);
-    public Vector3 Transform_NpcID_0_Scale = new Vector3(1, 1, 1);
-    public Vector3 Transform_NpcID_1_Position = new Vector3((float)-126.038147, (float)24.9887524, (float)237.177338);
-    public Vector3 Transform_NpcID_1_Rotation = new Vector3(0, 140, 0);
-    public Vector3 Transform_NpcID_1_Scale = new Vector3(1, 1, 1);
-    public Vector3 Transform_NpcID_2_Position = new Vector3((float)-60.0505524, (float)2.59646082, (float)97.9177856);
-    public Vector3 Transform_NpcID_2_Rotation = new Vector3(0, 75, 0);
-    public Vector3 Transform_NpcID_2_Scale = new Vector3(1, 1, 1);
-    public Vector3 Transform_NpcID_3_Position = new Vector3((float)-70.5333099, (float)-0.279643297, (float)21.70224);
-    public Vector3 Transform_NpcID_3_Rotation = new Vector3(0, 60, 0);
-    public Vector3 Transform_NpcID_3_Scale = new Vector3(1, 1, 1);
+    public Vector3 Transform_NpcID_0_Position = new Vector3(135.0f, 9.9981689453125f, 140.0f);
+    public Vector3 Transform_NpcID_0_Rotation = new Vector3(0.0f, 135.0f, 0.0f);
+    public Vector3 Transform_NpcID_0_Scale = new Vector3(1.0f, 1.0f, 1.0f);
+
+    public Vector3 Transform_NpcID_1_Position = new Vector3(136.5f, 9.998172760009766f, 175.0f);
+    public Vector3 Transform_NpcID_1_Rotation = new Vector3(0.0f, 160.237f, 0.0f);
+    public Vector3 Transform_NpcID_1_Scale = new Vector3(1.0f, 1.0f, 1.0f);
+
+    public Vector3 Transform_NpcID_2_Position = new Vector3(154.52000427246095f, 9.998165130615235f, 160.85000610351563f);
+    public Vector3 Transform_NpcID_2_Rotation = new Vector3(0.0f, -140.911f, 0.0f);
+    public Vector3 Transform_NpcID_2_Scale = new Vector3(1.0f, 1.0f, 1.0f);
+
+    public Vector3 Transform_NpcID_3_Position = new Vector3(148.24000549316407f, 9.9981689453125f, 177.60000610351563f);
+    public Vector3 Transform_NpcID_3_Rotation = new Vector3(0.0f, -150.0f, 0.0f);
+    public Vector3 Transform_NpcID_3_Scale = new Vector3(1.0f, 1.0f, 1.0f);
 }
