a1ebc12a235191c6460f69ce54dc3199b5f45e1f
Gaolingx <947770192@qq.com>
2024-04-29 19:08:10 +0800

    Update Materials

--- ET/Unity/Assets/StylizedEnvironement/Script/SkyboxRotation.cs
+++ ET/Unity/Assets/StylizedEnvironement/Script/SkyboxRotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skybox : MonoBehaviour
+public class SkyboxRotation : MonoBehaviour
 {
     public float Skyboxspeed;
     // Start is called before the first frame update
