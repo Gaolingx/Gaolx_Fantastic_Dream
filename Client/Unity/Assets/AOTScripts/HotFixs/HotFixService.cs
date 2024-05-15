67d6ddb9c90f46566217578128a2daed745b2bd1
Gaolingx <947770192@qq.com>
2024-05-15 18:31:23 +0800

    Revert some changes

--- Client/Unity/Assets/AOTScripts/HotFixs/HotFixService.cs
+++ Client/Unity/Assets/AOTScripts/HotFixs/HotFixService.cs
@@ -334,4 +334,41 @@ public class HotFixService : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 远端资源地址查询服务类
+    /// </summary>
+    private class RemoteServices : IRemoteServices
+    {
+        private readonly string _defaultHostServer;
+        private readonly string _fallbackHostServer;
+
+        public RemoteServices(string defaultHostServer, string fallbackHostServer)
+        {
+            _defaultHostServer = defaultHostServer;
+            _fallbackHostServer = fallbackHostServer;
+        }
+        string IRemoteServices.GetRemoteMainURL(string fileName)
+        {
+            return $"{_defaultHostServer}/{fileName}";
+        }
+        string IRemoteServices.GetRemoteFallbackURL(string fileName)
+        {
+            return $"{_fallbackHostServer}/{fileName}";
+        }
+    }
+
+    /// <summary>
+    /// 默认的分发资源查询服务类
+    /// </summary>
+    private class DefaultDeliveryQueryServices : IDeliveryQueryServices
+    {
+        public DeliveryFileInfo GetDeliveryFileInfo(string packageName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+        public bool QueryDeliveryFiles(string packageName, string fileName)
+        {
+            return false;
+        }
+    }
 }
