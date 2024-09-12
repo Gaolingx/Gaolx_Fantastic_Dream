0fb6b31c9f132db19ca2c9d933192632fea139e6
Gaolingx <947770192@qq.com>
2024-09-12 19:55:21 +0800

    update

--- Client/Unity/Assets/GameMain/Scripts/UIWindow/FubenWnd.cs
+++ Client/Unity/Assets/GameMain/Scripts/UIWindow/FubenWnd.cs
@@ -33,7 +33,7 @@ namespace DarkGod.Main
             //根据当前副本进度控制图标显示（只显示当前待完成副本的图标）
             for (int i = 0; i < fbBtnArr.Count; i++)
             {
-                fbBtnArr[i].onClick.AddListener(delegate { ClickTaskBtn((10000 - fbBtnArr.Count) + i); });
+                fbBtnArr[i].onClick.AddListener(delegate { ClickTaskBtn((10001 - fbBtnArr.Count) + i); });
                 if (i < fbid % 10000)
                 {
                     SetActive(fbBtnArr[i].gameObject);
